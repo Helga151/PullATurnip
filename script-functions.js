bc6eba18a0f999b370376aa36a82932af0dc4d5b
Helga151 <ogerlich151@gmail.com>
2021-08-30 15:46:12 +0200

    turnipDrawingsAdded

--- script-functions.js
+++ script-functions.js
@@ -2,6 +2,7 @@ function getRandom(min, max) {
     return Math.random() * (max - min) + min;
 }
 
+//defining what happen after clicking a button
 function search() {
     let value = parseInt(this.classList[1]);
     if (this.classList.contains('locked')) {
@@ -49,40 +50,50 @@ function search() {
 function getNewTurnip() {
     let turnip = document.createElement("span");
     let turipLifeDuration = getRandom(minTuripLifeDuration, maxTuripLifeDuration);
-    let height = 200;
-    let width = 200;
+    
+    let minWidth = Math.max(minTurnipWidth * getRandom(0.02, 0.03), 40); //width can't be smaller than 40px
+    let maxWidth = Math.max(minTurnipWidth * getRandom(0.02, 0.03) * getRandom(1, 2.25), 60);
+    let width = getRandom(minWidth, maxWidth);
+    let height = width * 2; //I made drawing that height is nearly 2 times longer that width
+    console.log({minWidth, maxWidth, width});
+    
     turnip.style.height = `${height}px`;
     turnip.style.width = `${width}px`;
 
-    let maxHeight = Math.max(window.innerHeight - height, 0);
-    let maxWidth = Math.max(window.innerWidth - width, 0);
+    //position of a turnip; to not touch the edges I took a 10px border
+    let maxTop = Math.max(window.innerHeight - height - 10, 10);
+    let maxLeft = Math.max(window.innerWidth - width - 10, 10);
 
-    turnip.style.top = `${Math.floor(getRandom(0, maxHeight))}px`;
-    turnip.style.left = `${Math.floor(getRandom(0, maxWidth))}px`;
+    turnip.style.top = `${Math.floor(getRandom(10, maxTop))}px`;
+    turnip.style.left = `${Math.floor(getRandom(10, maxLeft))}px`;
+    turnip.style.transform = `rotate(${getRandom(-45, 45)}deg)`;
 
     let turnipUrl = turnipPictures[Math.floor(getRandom(0, turnipPictures.length))];
-    console.log({turnipUrl});
+    //console.log({turnipUrl});
     turnip.style.backgroundImage = `url(${turnipUrl})`;
 
     turnip.classList.add('turnip');
     turnip.addEventListener('click', onClickAddScore);
+    
+    //setting turnip life duration
     setTimeout(() => {
         turnip.remove();
-    },turipLifeDuration);
+    }, turipLifeDuration);
     currentObjects.appendChild(turnip);
 }
 
-//adding score
+//adding score after clicking on a turnip
 function onClickAddScore() {
     score += Math.floor(getRandom(minBonus, maxBonus + 1));
     scoreText.textContent = score;
     this.remove();
 }
 
+//setting how fast a next turnip will be added to the screen
 function setRandomTurnipTimeout() {
     getNewTurnip();
-    let timeout = getRandom(1000, 5000);
-    console.log(timeout);
+    let timeout = getRandom(minTuripTimeout, maxTuripTimeout);
+    console.log({timeout});
     setTimeout(setRandomTurnipTimeout, timeout);
 }
 
@@ -92,6 +103,8 @@ setTimeout(setRandomTurnipTimeout, getRandom(500, 3000));
 
 setInterval(() => {
     document.title = `${score} money - Clicker`; //update title
+    minTurnipWidth = window.innerWidth; //update this value in case window width was changed
+    //check if any button can be updated
     buttons.forEach(button => {
         var value = parseInt(button.classList[1]);
         if (button.classList.contains('locked')) {
--- script-variables.js
+++ script-variables.js
@@ -5,23 +5,32 @@ const perSecondText = document.querySelector('.per-second');
 const currentObjects = document.querySelector('.current-objects');
 
 var score = 0;
-var perSecond = 0;
+var perSecond = 0; //money added to score per second
 var perSecondIntervalTime = 1000;
 var perSecondIntervalId;
-var multiplierCost = 2;
-var multiplierPerSecond = 0.01;
-var minBonus = 1;
+var multiplierCost = 2; //what will be the cost of another update
+var multiplierPerSecond = 0.01; //multiplier of button values defining how much money per second will be added to score
+//min and max bonus for clicking on turnip picture
+var minBonus = 1; 
 var maxBonus = 2;
-var minTuripLifeDuration = 1000;
-var maxTuripLifeDuration = 3000;
+//how long a turnip will be on the screen
+var minTuripLifeDuration = 5000;
+var maxTuripLifeDuration = 10000;
+//how fast a next turnip will be added to the screen
+var minTuripTimeout = 500;
+var maxTuripTimeout = 3000;
+//min width property of a turnip - it will be a random % from window width
+var minTurnipWidth = window.innerWidth;
 
+//an array of turnip drawings
 var turnipPictures = new Array(
-    "https://img.redro.pl/fototapety/rzepa-400-35388854.jpg",
-    "https://media.istockphoto.com/photos/turnip-picture-id174687596",
-    "https://cdn.w600.comps.canstockphoto.com/rzepa-zbiory-fotografii_csp19305500.jpg",
-    "https://st2.depositphotos.com/1011480/6625/v/600/depositphotos_66253189-stock-illustration-turnip-vector-illustration-hand-drawn.jpg",
-    "https://sklep.swiatkwiatow.pl/images/companies/1/nasiona-rzepy.jpg",
-    "https://stalowezdrowie.pl/wp-content/uploads/2020/07/czarna-rzepa-696x428.jpg"
+    //"https://img.redro.pl/fototapety/rzepa-400-35388854.jpg",
+    //"https://media.istockphoto.com/photos/turnip-picture-id174687596",
+    //"https://cdn.w600.comps.canstockphoto.com/rzepa-zbiory-fotografii_csp19305500.jpg",
+    //"https://st2.depositphotos.com/1011480/6625/v/600/depositphotos_66253189-stock-illustration-turnip-vector-illustration-hand-drawn.jpg",
+    //"https://sklep.swiatkwiatow.pl/images/companies/1/nasiona-rzepy.jpg",
+    "images/violet_white_wide_turnip.png",
+    "images/white_turnip.png"
 );
 
 
